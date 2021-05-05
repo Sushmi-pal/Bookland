70bda1e7368d42eb5ac3aa5f5a9f005b649896ec
Sushmita <sushmipalikhe97@gmail.com>
2021-05-05 14:08:51 +0545

    [Created README file]

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,20 +1,20 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.forms import ModelForm
+from django.forms import ModelForm,TextInput
 from .models import Profile
 
 
 class LoginForm(forms.Form):
-    username=forms.CharField(max_length=150)
-    password=forms.CharField(max_length=128,widget=forms.PasswordInput())
+    username=forms.CharField(max_length=150,widget=TextInput(attrs={'class': 'form-control'}))
+    password=forms.CharField(max_length=128,widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
 class SignupForm(forms.Form):
-    username=forms.CharField(max_length=150)
-    first_name=forms.CharField(max_length=150)
-    last_name=forms.CharField(max_length=150)
-    email=forms.CharField(max_length=128)
-    password=forms.CharField(max_length=128,widget=forms.PasswordInput())
-    confirm_password=forms.CharField(max_length=128,widget=forms.PasswordInput())
+    username=forms.CharField(max_length=150,widget=TextInput(attrs={'class': 'form-control'}))
+    first_name=forms.CharField(max_length=150,widget=TextInput(attrs={'class': "form-control"}))
+    last_name=forms.CharField(max_length=150,widget=TextInput(attrs={'class': 'form-control'}))
+    email=forms.CharField(max_length=128,widget=TextInput(attrs={'class': 'form-control'}))
+    password=forms.CharField(max_length=128,widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    confirm_password=forms.CharField(max_length=128,widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
     def clean_username(self):
         if User.objects.filter(username=self.cleaned_data['username']).exists():
@@ -29,10 +29,14 @@ class SignupForm(forms.Form):
             raise forms.ValidationError('Password donot match')
 
 class UserUpdateForm(forms.ModelForm):
-    email=forms.CharField(max_length=128)
+    email=forms.CharField(max_length=128,widget=TextInput(attrs={'class': 'form-control'}))
     class Meta:
         model=User
         fields=['username','email']
+        widgets={
+            'username':forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.TextInput(attrs={'class': 'form-control'}),
+            }
 
 
 class ProfileUpdateForm(forms.ModelForm):
